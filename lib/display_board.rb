ed82b97ce181577d802fb540275c297c102d9612
Luba Kassurova <kassurova@icloud.com>
2021-12-31 16:38:03 -0500

    commit tree 61569183b69d532773603403d323255f17cfa9e6

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,53 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board(board)
+    puts "   |   |   "
+    puts "-----------"
+    puts "   | X |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+board = ["O"," "," "," "," "," "," "," "," "]
+def display_board(board)
+    puts " O |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+board = ["O"," "," "," ","X"," "," "," "," "]
+def display_board(board)
+    puts " O |   |   "
+    puts "-----------"
+    puts "   | X |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+board = ["X","X","X"," "," "," "," "," "," "]
+def display_board(board)
+    puts " X | X | X "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+end
+board = ["O"," "," "," "," "," "," "," "," "]
+def display_board(board)
+
+
+puts  " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts  "-----------"
+  puts  " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts  "-----------"
+  puts  " #{board[6]} | #{board[7]} | #{board[8]} "
+end
